477fdafae86d1504881a0d59b4aaabac04e35c20
scx <shengniming@gmail.com>
2024-04-17 20:38:04 +0800

    complete function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
 "use client";
 
-import { useState } from "react";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
     const [input, setInput] = useState<string>("");
+    const [searchResults, setSearchResults] = useState<{
+        results: string[];
+        duration: number;
+    }>();
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!input) {
+                return setSearchResults(undefined);
+            }
+            const res = await fetch(`/api/search?q=${input}`);
+            const data = (await res.json()) as { results: string[]; duration: number };
+            setSearchResults(data);
+        };
+        fetchData();
+    }, [input]);
     return (
         <main className="h-screen w-screen grainy">
             <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
@@ -13,11 +37,44 @@ export default function Home() {
                     Type a query below and get your results in miliseconds.
                 </p>
                 <div className="max-w-md w-full">
-                    <input
-                        type="text"
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                    />
+                    <Command>
+                        <CommandInput
+                            value={input}
+                            onValueChange={setInput}
+                            placeholder="search countries... "
+                            className="placeholder:text-zinc-500"
+                        />
+
+                        <CommandList>
+                            {searchResults?.results.length === 0 ? (
+                                <CommandEmpty>No results found.</CommandEmpty>
+                            ) : null}
+
+                            {searchResults?.results ? (
+                                <CommandGroup heading="Results">
+                                    {searchResults?.results.map((result) => (
+                                        <CommandItem
+                                            key={result}
+                                            value={result}
+                                            onSelect={setInput}
+                                        >
+                                            {result}
+                                        </CommandItem>
+                                    ))}
+                                </CommandGroup>
+                            ) : null}
+                            {searchResults?.results ? (
+                                <>
+                                    <div className="h-px w-full bg-zinc-200" />
+
+                                    <p className="p-2 text-xs text-zinc-500">
+                                        Found {searchResults.results.length} results in{" "}
+                                        {searchResults?.duration.toFixed(0)}ms
+                                    </p>
+                                </>
+                            ) : null}
+                        </CommandList>
+                    </Command>
                 </div>
             </div>
         </main>
